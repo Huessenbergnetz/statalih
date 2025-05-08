0bd3ba99a7abe142cc2fe41c5e8888c94296d940
Buschmann <mf@huessenbergnetz.de>
2025-05-08 19:49:12 +0200

    [cmd] add files for feeds list-items command

---
+++ cmd/commands/feedslistitemscommand.cpp
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2025 Matthias Fehring <https://www.huessenbergnetz.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "feedslistitemscommand.h"
+
+
+FeedsListItemsCommand::FeedsListItemsCommand()
+{
+
+}
---
+++ cmd/commands/feedslistitemscommand.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2025 Matthias Fehring <https://www.huessenbergnetz.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef HBNST_FEEDSLISTITEMS_H
+#define HBNST_FEEDSLISTITEMS_H
+
+
+class FeedsListItemsCommand
+{
+public:
+    FeedsListItemsCommand();
+};
+
+#endif // HBNST_FEEDSLISTITEMS_H
